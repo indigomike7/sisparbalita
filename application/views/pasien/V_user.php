f4db687b083bb2b8faccc5ec2cda3e0399b27498
indigomike7 <indigomike7@gmail.com>
2021-06-17 09:55:01 +0700

    remove pasien adding in admin

--- application/views/pasien/V_user.php
+++ application/views/pasien/V_user.php
@@ -4,7 +4,7 @@
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800"><?= $sub_judul; ?></h1>
-        <a href="<?= base_url('klasifikasi/tambah'); ?>" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-plus fa-sm"></i> Tambah</a>
+<!--        <a href="<?= base_url('klasifikasi/tambah'); ?>" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-plus fa-sm"></i> Tambah</a>-->
     </div>
 
     <!-- notifikasi -->
